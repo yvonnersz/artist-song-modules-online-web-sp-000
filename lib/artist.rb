b168f8db60bc57831ecfca229acb8852a63e5b9c
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-10 12:54:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,10 +16,10 @@ class Artist
 
   @@artists = []
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+  # def initialize
+  #   @@artists << self
+  #   @songs = []
+  # end
 
   # def self.find_by_name(name)
   #   @@artists.detect{|a| a.name == name}
