ee1c355e63054ff87f90ed58bd3398fc172cc5ac
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-10 12:56:04 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,18 +2,18 @@
 module Memorable
   module ClassMethods
 
-  def reset_all
-    self.all.clear
-  end
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
+    def count
+      self.all.count
+    end
   end
-end
 
   module InstanceMethods
-    def initialize
-      self.class.all << self
-    end
+      def initialize
+        self.class.all << self
+      end
   end
 end
